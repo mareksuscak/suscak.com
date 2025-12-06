ebee225947b6918ac1186517ce433500cacc8dc3
mareksuscak <marek.suscak@gmail.com>
2025-12-06 18:02:46 +0100

    Modernize

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '~/prisma/client';
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -10,9 +11,14 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+});
+
 const prisma =
   global.prisma ||
   new PrismaClient({
+    adapter,
     log: ['query'],
   })
 
